04c99079f806c842ca1d0765f4cc4c16e55ba3e0
David Flynn <David.Flynn@unisys.com>
2019-09-04 11:38:49 +1200

    Added 2PC Console App. Just HelloWorld so far.

---
+++ 2PC.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2PC.App
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
